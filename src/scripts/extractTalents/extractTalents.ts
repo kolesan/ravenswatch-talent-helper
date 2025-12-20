8a54ec02539e131485167437b5a5a43386432357
kolesan <kolesan90@gmail.com>
2025-12-20 19:06:09 +0200

    extract geppetto talents

--- src/scripts/extractTalents/extractTalents.ts
+++ src/scripts/extractTalents/extractTalents.ts
@@ -1,48 +1,5 @@
-import { readFileSync } from "fs";
-import { writeFile } from "fs/promises";
-import { JSDOM } from "jsdom";
-
 import { heroes } from "../../data/heroes";
-import { heroTalentsFileName } from "../utils/heroTalentsFileName";
-import { heroTalentTableFileName } from "../utils/heroTalentTableFileName";
 
-import { fixApostrophes } from "./utils/fixApostrophes";
-import { parseBeowulfTable } from "./utils/parseBeowulfTable/parseBeowulfTable";
-import { parseCommonTable } from "./utils/parseCommonTable";
+import { extractHeroTalentsToFile } from "./utils/extractHeroTalentsToFile";
 
-heroes.asArray.slice(0, 3).forEach(hero => {
-    const fileName = heroTalentTableFileName(hero);
-    const fileText = readFileSync(fileName, 'utf-8');
-    const fileDom = new JSDOM(fileText);
-
-    const tableElem = fileDom.window.document
-        .querySelector(".fandom-table");
-
-    if (!tableElem) {
-        console.log(`Error, no talents table could be read for '${hero.name}'`);
-        return;
-    }
-
-    const rows = [...tableElem.querySelectorAll("tr")].slice(1);
-
-    const rowsToParse = rows;
-
-    const talents = rowsToParse.map(it => {
-        if (hero.name === "Beowulf") {
-            return parseBeowulfTable(it);
-        }
-        return parseCommonTable(it);
-    }).map(fixApostrophes);
-
-    console.log(rowsToParse.map(it => it.innerHTML));
-    console.log(talents);
-
-    writeFile(heroTalentsFileName(hero), JSON.stringify(talents, null, "    "))
-        .then(() => console.log(
-            `Success writing '${hero.name}' talents to file`
-        ))
-        .catch(err => console.log(
-            `Error writing '${hero.name}' talents to file: `, 
-            err
-        ));
-});
+extractHeroTalentsToFile(heroes.all.geppetto);
---
+++ src/scripts/extractTalents/utils/extractHeroTalentsToFile.ts
@@ -0,0 +1,48 @@
+import { readFileSync } from "fs";
+import { writeFile } from "fs/promises";
+import { JSDOM } from "jsdom";
+
+import { Hero } from "../../../data/heroes";
+import { heroTalentsFileName } from "../../utils/heroTalentsFileName";
+import { heroTalentTableFileName } from "../../utils/heroTalentTableFileName";
+
+import { fixApostrophes } from "./fixApostrophes";
+import { parseBeowulfTable } from "./parseBeowulfTable/parseBeowulfTable";
+import { parseCommonTable } from "./parseCommonTable";
+
+export function extractHeroTalentsToFile(hero: Hero) {
+    const fileName = heroTalentTableFileName(hero);
+    const fileText = readFileSync(fileName, 'utf-8');
+    const fileDom = new JSDOM(fileText);
+
+    const tableElem = fileDom.window.document
+        .querySelector(".fandom-table");
+
+    if (!tableElem) {
+        console.log(`Error, no talents table could be read for '${hero.name}'`);
+        return;
+    }
+
+    const rows = [...tableElem.querySelectorAll("tr")].slice(1);
+
+    const rowsToParse = rows;
+
+    const talents = rowsToParse.map(it => {
+        if (hero.name === "Beowulf") {
+            return parseBeowulfTable(it);
+        }
+        return parseCommonTable(it);
+    }).map(fixApostrophes);
+
+    console.log(rowsToParse.map(it => it.innerHTML));
+    console.log(talents);
+
+    writeFile(heroTalentsFileName(hero), JSON.stringify(talents, null, "    "))
+        .then(() => console.log(
+            `Success writing '${hero.name}' talents to file`
+        ))
+        .catch(err => console.log(
+            `Error writing '${hero.name}' talents to file: `, 
+            err
+        ));
+}
