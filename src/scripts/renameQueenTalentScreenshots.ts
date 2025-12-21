c3a6d00e0ce55f8496e9ab9d406150106b3c5c66
kolesan <kolesan90@gmail.com>
2025-12-21 20:41:00 +0200

    save script for screenshot renaming as an example

---
+++ src/scripts/renameQueenTalentScreenshots.ts
@@ -0,0 +1,15 @@
+import { renameSync } from "fs";
+import { snowQueenOrder } from "./extractTalents/utils/applyIngameOrder/consts/snowQueenOrder";
+import { listDirFilesSyncRecursive } from "./parseTalentScreenshots/utils/listDirFilesSyncRecursive";
+
+const snowQueenScreenshots = listDirFilesSyncRecursive("src\\data\\screenshots\\snowQueen");
+
+console.log(snowQueenScreenshots);
+console.log(snowQueenOrder);
+
+snowQueenScreenshots.forEach((screenshot, i) => {
+    renameSync(
+        screenshot, 
+        screenshot.replace(/snowQueen\\.+\.png/, `snowQueen\\${snowQueenOrder[i]}.png`)
+    )
+});
