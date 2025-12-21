68c22055dd880c662b2f4ac17a6d68e64784a501
kolesan <kolesan90@gmail.com>
2025-12-21 19:38:29 +0200

    apply all hero orders

--- src/scripts/extractTalents/utils/applyIngameOrder/applyIngameOrder.ts
+++ src/scripts/extractTalents/utils/applyIngameOrder/applyIngameOrder.ts
@@ -1,14 +1,30 @@
 import { Hero } from "../../../../data/heroes";
 import { Talent } from "../../types";
-import { beowulfOrder } from "./consts/beowulfOrder";
-import { piperOrder } from "./consts/piperOrder";
 
+import { aladdinOrder } from "./consts/aladdinOrder";
+import { beowulfOrder } from "./consts/beowulfOrder";
+import { carmillaOrder } from "./consts/carmillaOrder";
+import { geppettoOrder } from "./consts/geppettoOrder";
+import { julietOrder } from "./consts/julietOrder";
+import { melusineOrder } from "./consts/melusineOrder";
+import { piperOrder } from "./consts/piperOrder";
+import { romeoOrder } from "./consts/romeoOrder";
 import { scarletOrder } from "./consts/scarletOrder";
+import { snowQueenOrder } from "./consts/snowQueenOrder";
+import { wukongOrder } from "./consts/wukongOrder";
 
 const orderMap: Partial<Record<Hero["code"], string[]>> = {
     scarlet: scarletOrder,
     piper: piperOrder,
     beowulf: beowulfOrder,
+    snowQueen: snowQueenOrder,
+    aladdin: aladdinOrder,
+    melusine: melusineOrder,
+    geppetto: geppettoOrder,
+    wukong: wukongOrder,
+    carmilla: carmillaOrder,
+    romeo: romeoOrder,
+    juliet: julietOrder,
 }
 
 export function applyIngameOrder(hero: Hero) {
