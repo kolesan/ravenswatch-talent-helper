b8a04321a29010e885953d23eec6ab7962239142
kolesan <kolesan90@gmail.com>
2025-12-21 21:34:46 +0200

    extract missing talent icons from screenshots for scarlet and piper

--- src/scripts/parseTalentScreenshots/parseTalentScreenshots.ts
+++ src/scripts/parseTalentScreenshots/parseTalentScreenshots.ts
@@ -8,14 +8,29 @@ const files = listDirFilesSyncRecursive(baseScreenshotsDir);
 
 files.forEach(file => {
     console.log(`Extracting talent icon from '${file}'`);
-    sharp(file)
+    const sharpFile = sharp(file);
+
+    const extracted = sharpFile
         .extract({
             top: 843,
             left: 1108,
             width: 159,
             height: 154,
-        })
-        .toFile(outputFilePath(file))
+        });
+
+    const composited = extracted
+        .clone()
+        .composite([{
+            input: `${baseScreenshotsDir}\\empty_corner.webp`,
+            top: 8,
+            left: 1,
+        }]);
+    
+    const final = file.includes("locked_talent") || file.includes("empty_corner")
+        ? extracted
+        : composited;
+
+    final.toFile(outputFilePath(file))
         .then(() => console.log("Success"))
         .catch((err) => console.log("Error:", err));
 })
