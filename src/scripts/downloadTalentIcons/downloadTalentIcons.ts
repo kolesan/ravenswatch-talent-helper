634215ef05b2b6179d28182cfa8e7ebe8306e20d
kolesan <kolesan90@gmail.com>
2025-12-22 15:35:19 +0200

    download aladdin talent icons available on wiki

--- src/scripts/downloadTalentIcons/downloadTalentIcons.ts
+++ src/scripts/downloadTalentIcons/downloadTalentIcons.ts
@@ -7,7 +7,7 @@ import { runPromiseChain } from "../utils/runPromiseChain";
 
 import { fetchAndSaveIcon } from "./utils/fetchAndSaveIcon";
 
-const hero = heroes.all.geppetto;
+const hero = heroes.all.aladdin;
 
 const fileName = heroTalentsFileName(hero);
 const fileText = readFileSync(fileName, 'utf-8');
