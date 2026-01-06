a069a2f89d15d1d3c276e116e458d429b6b36be7
kolesan <kolesan90@gmail.com>
2026-01-06 20:24:29 +0200

    add index html to rollup config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
     build: {
         rollupOptions: {
             input: {
+                main: resolve(__dirname, 'index.html'),
                 talents: resolve(__dirname, 'talents.html'),
                 legendary: resolve(__dirname, 'legendary-objects.html'),
                 cursed: resolve(__dirname, 'cursed-objects.html'),
