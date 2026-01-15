988b1905b5d5502b3811ef14cc0773f025def1f5
kolesan <kolesan90@gmail.com>
2026-01-15 16:59:51 +0200

    extract navigation component

--- src/ui/components/Toolbar/Toolbar.ts
+++ src/ui/components/Toolbar/Toolbar.ts
@@ -1,30 +1,15 @@
-import { clsx } from "clsx";
 import { html } from "htm/preact";
-import { Link } from "wouter-preact";
-
-import { pages } from "../../../../pages";
 
 import { Logo } from "./components/Logo/Logo";
+import { Navigation } from "./components/Navigation/Navigation";
 
 import cls from "./Toolbar.module.css";
 
 export function Toolbar() {
     return html`
-        <div class=${cls.root}>
+        <div class=${cls.toolbarRoot}>
             <${Logo} />
-            <div class=${cls.navigation}>
-                ${pages.utils.asArray().map(it => html`
-                    <${Link} 
-                        className=${(active: boolean) => clsx({
-                            [cls.navigationItem]: true,
-                            [cls.navigationItemActive]: active,
-                        })}
-                        href=${it.path}
-                    >
-                        ${it.label}
-                    </${Link}>
-                `)}
-            </div>
+            <${Navigation} />
         </div>
     `;
 }
---
+++ src/ui/components/Toolbar/components/Navigation/Navigation.ts
@@ -0,0 +1,31 @@
+import { clsx } from "clsx";
+import { html } from "htm/preact";
+import { Link } from "wouter-preact";
+
+import { pages } from "../../../../../../pages";
+
+import cls from "./Navigation.module.css";
+
+type Props = {
+    className?: string;
+}
+
+export function Navigation({
+    className,
+}: Props) {
+    return html`
+        <div class=${clsx(cls.navigationRoot, className)}>
+            ${pages.utils.asArray().map(it => html`
+                <${Link} 
+                    className=${(active: boolean) => clsx({
+                        [cls.navigationItem]: true,
+                        [cls.navigationItemActive]: active,
+                    })}
+                    href=${it.path}
+                >
+                    ${it.label}
+                </${Link}>
+            `)}
+        </div>
+    `;
+}
