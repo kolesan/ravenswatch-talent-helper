68c6ea0e694dba41a3c069c47a89b30976b5b571
kolesan <kolesan90@gmail.com>
2026-01-19 11:54:00 +0200

    merlin patch changes to legendary objects

--- src/scrapedData/items/legendary.ts
+++ src/scrapedData/items/legendary.ts
@@ -2,10 +2,31 @@ import { MagicalObject } from "../../types";
 
 export const legendary: MagicalObject[] = [
     {
-        code: "philosopher's_stone",
-        name: "Philosopher's Stone",
+        code: "excalibur",
+        name: "Excalibur",
         description: [
-            "Gain +1 VITALITY each time you collect a Healing Orb"
+            "+10 DMG for each Legendary Talent you have",
+        ],
+    },
+    {
+        code: "father_time's_hourglass",
+        name: "Father Time's Hourglass",
+        description: [
+            "Reduces POWER, SPECIAL and DEFENSE cooldowns by 20%",
+        ],
+    },
+    {
+        code: "golden_coat_of_chainmail",
+        name: "Golden Coat of Chainmail",
+        description: [
+            "Damage taken within 1 second(s) cannot exceed 50% of your Maximum Health"
+        ],
+    },
+    {
+        code: "holy_grail",
+        name: "Holy Grail",
+        description: [
+            "When in combat, DEFENSE restores 8 health to all nearby heroes (At most every 5 seconds)"
         ],
     },
     {
@@ -16,6 +37,34 @@ export const legendary: MagicalObject[] = [
         ],
     },
     {
+        code: "philosopher's_stone",
+        name: "Philosopher's Stone",
+        description: [
+            "Gain +1 VITALITY each time you collect a Healing Orb"
+        ],
+    },
+    {
+        code: "raven_effigy",
+        name: "Raven effigy",
+        description: [
+            "POWER and SPECIAL gain +1 charge(s)"
+        ],
+    },
+    {
+        code: "ring_of_dispel",
+        name: "Ring of Dispel",
+        description: [
+            "DASH applies VULNERABLE for 4 seconds to enemies hit"
+        ],
+    },
+    {
+        code: "seven-league_boots",
+        name: "Seven-league boots",
+        description: [
+            "Remove DASH's cooldown whenever you use POWER, SPECIAL, or DEFENSE"
+        ],
+    },
+    {
         code: "vorpal_blade",
         name: "Vorpal Blade",
         description: [
@@ -23,11 +72,10 @@ export const legendary: MagicalObject[] = [
         ],
     },
     {
-        code: "water_of_life",
-        name: "Water of Life",
+        code: "swan_cloak",
+        name: "Swan Cloak",
         description: [
-            "+25 VITALITY",
-            "Fully Heal at the start of each DAY or NIGHT"
+            "DASH heals for 50% of damage taken over the last 2 second(s)"
         ],
     },
     {
@@ -39,59 +87,11 @@ export const legendary: MagicalObject[] = [
         ],
     },
     {
-        code: "holy_grail",
-        name: "Holy Grail",
+        code: "water_of_life",
+        name: "Water of Life",
         description: [
-            "When in combat, DEFENSE restores 8 health to all nearby heroes (At most every 5 seconds)"
-        ],
-    },
-    {
-        code: "swan_cloak",
-        name: "Swan Cloak",
-        description: [
-            "DASH heals for +50% of damage taken over the last 2 second(s)"
-        ],
-    },
-    {
-        code: "ring_of_dispel",
-        name: "Ring of Dispel",
-        description: [
-            "DASH applies VULNERABLE for 4 seconds to enemies hit"
-        ],
-    },
-    {
-        code: "excalibur",
-        name: "Excalibur",
-        description: [
-            "+10 DMG for each Legendary Talent you have"
-        ],
-    },
-    {
-        code: "seven-league_boots",
-        name: "Seven-league boots",
-        description: [
-            "Remove DASH's cooldown whenever you use POWER, SPECIAL, or DEFENSE"
-        ],
-    },
-    {
-        code: "golden_coat_of_chainmail",
-        name: "Golden Coat of Chainmail",
-        description: [
-            "Damage taken within 1 second(s) cannot exceed 50% of your Maximum Health"
-        ],
-    },
-    {
-        code: "raven_effigy",
-        name: "Raven effigy",
-        description: [
-            "POWER and SPECIAL gain +1 charge(s)"
-        ],
-    },
-    {
-        code: "father_time's_hourglass",
-        name: "Father Time's Hourglass",
-        description: [
-            "POWER, SPECIAL and DEFENSE have a 10% reduced cooldown for each 20% of Health missing"
+            "+25 VITALITY",
+            "Prevents lethal damage and restores all health instead (once per chapter)"
         ],
     },
 ];
