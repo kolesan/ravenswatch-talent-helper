5bc26e9314d9f3939a2a7eda1c39d496689e0daf
kolesan <kolesan90@gmail.com>
2026-01-21 20:42:12 +0200

    add a switcher for showing full talent encyclopedia view

--- src/ui/pages/TalentsPage/TalentsPage.ts
+++ src/ui/pages/TalentsPage/TalentsPage.ts
@@ -1,13 +1,14 @@
 import clsx from "clsx";
 import { html } from "htm/preact";
 import { useEffect } from "preact/hooks";
-import { useParams } from "wouter-preact";
+import { useParams, useSearchParams } from "wouter-preact";
 
 import { Hero, heroes } from "../../../finalData/finalData";
 import { Talent } from "../../../scripts/extractTalents/types";
 import { hst } from "../../core/hst";
 import { usePageTitle } from "../../hooks/usePageTitle";
 
+import { Encyclopedia } from "./components/Encyclopedia/Encyclopedia";
 import { HeroSelect } from "./components/HeroSelect/HeroSelect";
 import { MainList } from "./components/MainList/MainList";
 import { maxUsedTalents } from "./consts/maxUsedTalents";
@@ -25,6 +26,9 @@ import cls from "./TalentsPage.module.css";
 type RouteParams = {
     hero: string;
 }
+
+// TODO temp solution
+let viewAllGlobal = false;
 
 export function TalentsPage() {
     const params = useParams<RouteParams>();
@@ -57,11 +61,24 @@ export function TalentsPage() {
         heroCodeFromState: state.hero.code,
     });
 
+    // TODO Rework all of this
+    const [searchParams] = useSearchParams();
+    const viewAllFromSearch = searchParams.has("view-all");
+    useEffect(() => {
+        if (viewAllGlobal && !viewAllFromSearch) {
+            const query = "?view-all=true";
+            hst.replace(`${state.hero.code}${query}`);
+        } else if (viewAllFromSearch && !viewAllGlobal) {
+            viewAllGlobal = true;
+        }
+    }, [viewAllFromSearch]);
+
     if (!heroFromUrl) {
         return null;
     }
 
     const derivedTalentsState = getDerivedTalentsState(state);
+    const viewAll = viewAllFromSearch;
 
     return html`
         <div 
@@ -75,7 +92,9 @@ export function TalentsPage() {
                 items=${heroes.asArray}
                 value=${state.hero}
                 onChange=${(hero: Hero) => {
-                    hst.push(hero.code);
+                    // TODO Extract view-all to const and reuse
+                    const query = viewAll ? "?view-all=true" : "";
+                    hst.push(`${hero.code}${query}`);
                 }}
             />
             <label class=container-label>
@@ -96,111 +115,137 @@ export function TalentsPage() {
                 />
                 <output>${state.rank}</output>
             </label>
+            <label 
+                class="container-label wrapabale-label checkbox-label"
+                onClick=${() => {
+                    const newViewAll = !viewAll;
+                    viewAllGlobal = newViewAll;
+                    const query = newViewAll ? "?view-all=true" : "";
+                    hst.push(`${state.hero.code}${query}`);
+                }}
+            >
+                All<div>${viewAll ? "☑️" : "⬛"}</div>
+            </label>
         </div>
-        <div class=${cls.talentLists}>
-            <${MainList}
+        ${viewAll && html`
+            <${Encyclopedia} 
                 classes=${{ 
-                    label: cls.listLabel,
-                    content: cls.listContent,
-                }}
-                label=Used 
-                heroCode=${state.hero.code} 
-                talents=${state.talents.used} 
-                maxItems=${maxUsedTalents}
-                onTalentClick=${(talent: Talent) => {
-                    dispatch({
-                        type: talent.preferred
-                            ? "talent_from_used_to_preferred"
-                            : "talent_from_used_to_available",
-                        talent,
-                    });
-                }}
-                onTalentAltClick=${(talent: TalentWithLockedFlag) => {
-                    dispatch({
-                        type: talent.preferred
-                            ? "talent_from_used_to_preferred"
-                            : "talent_from_used_to_available",
-                        talent,
-                    });
-                }}
-                onTalentHold=${(talent: TalentWithLockedFlag) => {
-                    dispatch({
-                        type: talent.preferred
-                            ? "talent_from_used_to_preferred"
-                            : "talent_from_used_to_available",
-                        talent,
-                    });
-                }}
-            />
-            <${MainList} 
-                classes=${{ 
-                    label: cls.listLabel,
-                    content: cls.listContent,
-                }}
-                label=Preferred 
-                heroCode=${state.hero.code} 
-                talents=${state.talents.preferred} 
-                onTalentClick=${(talent: Talent) => {
-                    dispatch({
-                        type: "talent_from_preferred_to_used",
-                        talent,
-                    });
-                }}
-                onTalentAltClick=${(talent: Talent) => {
-                    dispatch({
-                        type: "talent_from_preferred_to_available",
-                        talent,
-                    });
-                }}
-                onTalentHold=${(talent: TalentWithLockedFlag) => {
-                    dispatch({
-                        type: "talent_from_preferred_to_available",
-                        talent,
-                    });
-                }}
-            />
-            <${MainList} 
-                classes=${{ 
-                    label: cls.listLabel,
-                    content: cls.listContent,
-                }}
-                label=Available 
-                heroCode=${state.hero.code} 
-                talents=${[
-                    ...derivedTalentsState.available, 
-                    ...derivedTalentsState.locked.map(markLocked),
-                ]}
-                onTalentClick=${(talent: TalentWithLockedFlag) => {
-                    if (talent.locked) {
-                        return;
+                    list: {
+                        label: cls.listLabel,
+                        content: cls.listContent,
                     }
+                }}
+                heroCode=${state.hero.code}
+                heroRank=${state.rank}
+                talents=${state.hero.talents}
+            />
+        `}
+        ${!viewAll && html`
+            <div class=${cls.talentLists}>
+                <${MainList}
+                    classes=${{ 
+                        label: cls.listLabel,
+                        content: cls.listContent,
+                    }}
+                    label=Used 
+                    heroCode=${state.hero.code} 
+                    talents=${state.talents.used} 
+                    maxItems=${maxUsedTalents}
+                    onTalentClick=${(talent: Talent) => {
+                        dispatch({
+                            type: talent.preferred
+                                ? "talent_from_used_to_preferred"
+                                : "talent_from_used_to_available",
+                            talent,
+                        });
+                    }}
+                    onTalentAltClick=${(talent: TalentWithLockedFlag) => {
+                        dispatch({
+                            type: talent.preferred
+                                ? "talent_from_used_to_preferred"
+                                : "talent_from_used_to_available",
+                            talent,
+                        });
+                    }}
+                    onTalentHold=${(talent: TalentWithLockedFlag) => {
+                        dispatch({
+                            type: talent.preferred
+                                ? "talent_from_used_to_preferred"
+                                : "talent_from_used_to_available",
+                            talent,
+                        });
+                    }}
+                />
+                <${MainList} 
+                    classes=${{ 
+                        label: cls.listLabel,
+                        content: cls.listContent,
+                    }}
+                    label=Preferred 
+                    heroCode=${state.hero.code} 
+                    talents=${state.talents.preferred} 
+                    onTalentClick=${(talent: Talent) => {
+                        dispatch({
+                            type: "talent_from_preferred_to_used",
+                            talent,
+                        });
+                    }}
+                    onTalentAltClick=${(talent: Talent) => {
+                        dispatch({
+                            type: "talent_from_preferred_to_available",
+                            talent,
+                        });
+                    }}
+                    onTalentHold=${(talent: TalentWithLockedFlag) => {
+                        dispatch({
+                            type: "talent_from_preferred_to_available",
+                            talent,
+                        });
+                    }}
+                />
+                <${MainList} 
+                    classes=${{ 
+                        label: cls.listLabel,
+                        content: cls.listContent,
+                    }}
+                    label=Available 
+                    heroCode=${state.hero.code} 
+                    talents=${[
+                        ...derivedTalentsState.available, 
+                        ...derivedTalentsState.locked.map(markLocked),
+                    ]}
+                    onTalentClick=${(talent: TalentWithLockedFlag) => {
+                        if (talent.locked) {
+                            return;
+                        }
 
-                    dispatch({
-                        type: "talent_from_available_to_used",
-                        talent,
-                    });
-                }}
-                onTalentAltClick=${(talent: TalentWithLockedFlag) => {
-                    if (talent.locked) {
-                        return;
-                    }
-                    
-                    dispatch({
-                        type: "talent_from_available_to_preferred",
-                        talent,
-                    });
-                }}
-                onTalentHold=${(talent: TalentWithLockedFlag) => {
-                    if (talent.locked) {
-                        return;
-                    }
-                    
-                    dispatch({
-                        type: "talent_from_available_to_preferred",
-                        talent,
-                    });
-                }}
-            />
-        </div>
+                        dispatch({
+                            type: "talent_from_available_to_used",
+                            talent,
+                        });
+                    }}
+                    onTalentAltClick=${(talent: TalentWithLockedFlag) => {
+                        if (talent.locked) {
+                            return;
+                        }
+                        
+                        dispatch({
+                            type: "talent_from_available_to_preferred",
+                            talent,
+                        });
+                    }}
+                    onTalentHold=${(talent: TalentWithLockedFlag) => {
+                        if (talent.locked) {
+                            return;
+                        }
+                        
+                        dispatch({
+                            type: "talent_from_available_to_preferred",
+                            talent,
+                        });
+                    }}
+                />
+            </div>
+        `}
     `;
 }
