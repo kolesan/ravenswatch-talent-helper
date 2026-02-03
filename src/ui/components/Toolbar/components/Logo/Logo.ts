f6ab6b65f25a7d1c5c2e22f9e243d31482c6cc0e
kolesan <kolesan90@gmail.com>
2026-02-03 18:24:48 +0200

    optimize logos

--- src/ui/components/Toolbar/components/Logo/Logo.ts
+++ src/ui/components/Toolbar/components/Logo/Logo.ts
@@ -18,14 +18,14 @@ export function Logo({
         >
             <img 
                 class=${cls.logoSmall}
-                src="/logos/logo-small.webp" 
+                src="/logos/optimized/logo-small.webp" 
                 width=94 
                 height=94 
             />
             <div class=${cls.logoBig}>
                 <img 
                     class=${cls.logoImg}
-                    src="/logos/ravenswatch-logo.webp" 
+                    src="/logos/optimized/ravenswatch-logo.webp" 
                     width=314 
                     height=80 
                 />
