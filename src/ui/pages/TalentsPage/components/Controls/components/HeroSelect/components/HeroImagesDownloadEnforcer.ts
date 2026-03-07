1aafdeaf6670282b910fd417bb71d2694a5cfd23
kolesan <kolesan90@gmail.com>
2026-03-07 13:07:33 +0200

    improve hero image enforcer a bit

--- src/ui/pages/TalentsPage/components/Controls/components/HeroSelect/components/HeroImagesDownloadEnforcer.ts
+++ src/ui/pages/TalentsPage/components/Controls/components/HeroSelect/components/HeroImagesDownloadEnforcer.ts
@@ -6,13 +6,26 @@ const style = (name: string) => `background-image: url("/art/newResampled/${name
 
 export function HeroImagesDownloadEnforcer() {
     return html`
-        <div style="visibility: hidden; height: 0; width: 0;">
+        <div 
+            style=${`
+                visibility: hidden; 
+                height: 0; 
+                width: 0; 
+                pointer-events: none; 
+                overflow: hidden;
+            `}
+        >
             ${heroCodes.map(it => html`
                 <div style=${style(it)}>
                 </div>
             `)}
             ${heroCodes.map(it => html`
-                <img src=${`/icons/heroes/optimized/${it}.webp`} height=0 />
+                <img 
+                    src=${`/icons/heroes/optimized/${it}.webp`} 
+                    height=0 
+                    width=0 
+                    decoding="async" 
+                />
             `)}
         </div>
     `;
