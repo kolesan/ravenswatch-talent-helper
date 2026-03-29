e75ad3db6b3699c140251b7b66944bcc682d4c36
kolesan <kolesan90@gmail.com>
2026-03-29 14:05:54 +0300

    cs

--- src/scripts/objects/generateMagicalObjects/generateMagicalObjects.ts
+++ src/scripts/objects/generateMagicalObjects/generateMagicalObjects.ts
@@ -2,11 +2,12 @@ import { parsePasstechItems } from "./utils/parsePasstechItems";
 import { passtechItemToMagicalObject } from "./utils/passtechItemToMagicalObject";
 import { writeMagicalObjectsToFile } from "./utils/writeMagicalObjectsToFile";
 
+const dir = "./src/data/objects";
+
 const passtechItems = await parsePasstechItems();
 
 const legendaryObjects = passtechItems.legendary.map(passtechItemToMagicalObject);
 const cursedObjects = passtechItems.cursed.map(passtechItemToMagicalObject);
 
-const dir = "./src/data/objects";
 await writeMagicalObjectsToFile(dir, "legendary", legendaryObjects);
 await writeMagicalObjectsToFile(dir, "cursed", cursedObjects);
