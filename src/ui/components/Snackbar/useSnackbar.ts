33b3afb78468a6e746df570dcff4abb28e6e85bb
kolesan <kolesan90@gmail.com>
2026-06-01 17:55:00 +0300

    fix an issue with hidden snackbar blocking user input

--- src/ui/components/Snackbar/useSnackbar.ts
+++ src/ui/components/Snackbar/useSnackbar.ts
@@ -3,10 +3,13 @@ import { useState } from "preact/hooks";
 import { useBooleanState } from "ui/hooks/useBooleanState";
 import { useVar } from "ui/hooks/useVar";
 
+const hideBlockDurationMs = 150;
+
 export function useSnackbar() {
     const open = useBooleanState(false);
     const [text, setText] = useState("");
     const [getHideTimeoutId, setHideTimeoutId] = useVar<NodeJS.Timeout | null>(null);
+    const [getShownAtTimestamp, setShownAtTimestamp] = useVar<number | null>(null);
 
     const clearSnackHideTimeout = () => {
         const lastSnackHideTimeoutId = getHideTimeoutId();
@@ -21,12 +24,24 @@ export function useSnackbar() {
         setText(msg);
         open.on();
         setHideTimeoutId(setTimeout(open.off, 2400));
+        setShownAtTimestamp(Date.now());
     }
 
     return {
         open: open.is,
         text,
         show,
-        hide: open.off,
+        hide: () => {
+            // Without this check the snackbar gets shown and instantly hidden
+            // if the user clicks a list item under a hidden snackbar
+            const lastShownAt = getShownAtTimestamp();
+            const currentTime = Date.now();
+            const canHide = lastShownAt === null
+                || currentTime - lastShownAt > hideBlockDurationMs;
+
+            if (canHide) {
+                open.off();
+            }
+        },
     }
 }
